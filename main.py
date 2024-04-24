0b7c8a315f472b356a74e3cd7b40f4cd8df4f793
Kenki <lukassofka@gmail.com>
2024-04-24 14:34:14 +0200

    "Refactored Game class and added spaceship health; added projectile groups for invaders and spaceship; updated shoot\_projectile and game update functions"

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import pygame
+import pygame, sys, random
 from pygame.locals import *
-import sys
-import random
 from assets import *
 from pygame.sprite import Sprite, Group
 
@@ -22,15 +20,44 @@ BASIC_FONT_SIZE = 30
 BUTTONS = None
 
 
-# Class for SpaceShip 
-class SpaceShip(pygame.sprite.Sprite): # Here we are inheriting from the pygame.sprite.Sprite class
+# Class for Game
+class Game:
+    def __init__(self, Invader_group, SpaceShip, Projectile_group_inv, Projectile_group_spa):
+        # Create a game class with all the groups
+        self.invader_group = Invader_group
+        self.projectile_group_invaders = Projectile_group_inv
+        self.projectile_group_spaceship = Projectile_group_spa
+        self.spaceship = SpaceShip
+        self.game_active = True
+
+    def update(self, DISPLAY_SURFACE):
+        for invader in self.invader_group:
+            invader.update(self)
+
+        self.projectile_movement()
+        spaceship_collision(self)
+
+    def projectile_movement(self):
+        if self.game_active:
+            self.projectile_group_invaders.update()
+            self.projectile_group_invaders.draw(DISPLAY_SURFACE)
+
+            self.projectile_group_spaceship.update()
+            self.projectile_group_spaceship.draw(DISPLAY_SURFACE)
+
+
+# Class for SpaceShip
+class SpaceShip(pygame.sprite.Sprite):  # Here we are inheriting from the pygame.sprite.Sprite class
     def __init__(self):
         super().__init__()
         # The order of self.image: load image -> resize image -> rotate image
-        self.image = pygame.transform.rotate(pygame.transform.scale(pygame.image.load(SPACESHIP_IMG).convert_alpha(), (int(WINDOW_WIDTH * 0.2), int(WINDOW_WIDTH * 0.2))), 180.0)
+        self.image = pygame.transform.rotate(pygame.transform.scale(pygame.image.load(SPACESHIP_IMG).convert_alpha(), (
+        int(WINDOW_WIDTH * 0.18), int(WINDOW_WIDTH * 0.18))), 180.0)
         # The order of self.rect: get rect from image -> set rect position
-        self.rect = self.image.get_rect(midbottom=(WINDOW_WIDTH/2, WINDOW_HEIGHT-75))
-    
+        self.rect = self.image.get_rect(midbottom=(WINDOW_WIDTH / 2, WINDOW_HEIGHT - 75))
+        self.health = 3
+
+
 # Class for Invader 
 class Invader(pygame.sprite.Sprite):
     def __init__(self, x_pos, y_pos):
@@ -38,34 +65,33 @@ class Invader(pygame.sprite.Sprite):
         self.x = x_pos
         self.y = y_pos
         # Same order as SpaceShip
-        self.image = pygame.transform.scale(pygame.image.load(INVADER_IMG).convert_alpha(), (int(WINDOW_WIDTH * 0.10), int(WINDOW_WIDTH * 0.10)))
+        self.image = pygame.transform.scale(pygame.image.load(INVADER_IMG).convert_alpha(),
+                                            (int(WINDOW_WIDTH * 0.10), int(WINDOW_WIDTH * 0.10)))
         self.rect = self.image.get_rect(midbottom=(self.x, self.y))
+
+    def update(self, game: Game):
+        if random.randint(0, 1000) == 1:
+            # Create a projectile
+            shoot_projectile(game=game, position=(self.x, self.y), direction=1, speed=5)
+
 
 # Class for Projectile
 class Projectile(pygame.sprite.Sprite):
-    def __init__(self, position):
+    def __init__(self, position, direction, speed):
         super().__init__()
         # create a surface with the size of 5x10
         self.image = pygame.Surface((5, 10))
         # fill the surface with red color
         self.image.fill((255, 0, 0))
         self.rect = self.image.get_rect(midbottom=position)
-        self.speed = 10 
+        self.speed = direction * speed
 
     def update(self):
         """"Move projectile by given distance of speed"""
-        self.rect.y -= self.speed
+        self.rect.y += self.speed
         # If projectile goes off the screen, remove it
-        if self.rect.bottom < 0:
+        if self.rect.bottom < 0 or self.rect.top > WINDOW_HEIGHT:
             self.kill()  # Remove the projectile if it goes off the screen
-
-# Class for Game
-class Game:
-    def __init__(self, Invader_group, SpaceShip: SpaceShip, Projectile_group):
-        # Create a game class with all the groups
-        self.invader_group = Invader_group
-        self.projectile_group = Projectile_group
-        self.spaceship = SpaceShip
 
 
 def handle_keypress(event, game: Game):
@@ -75,30 +101,44 @@ def handle_keypress(event, game: Game):
     elif event == pygame.K_RIGHT:
         game.spaceship.rect.x += 10
     elif event == pygame.K_SPACE:
-        shoot_projectile(game)
+        if not game.projectile_group_spaceship.sprites():
+            shoot_projectile(game, position=game.spaceship.rect.midtop, direction=-1)
 
 
-def shoot_projectile(game: Game):
+def shoot_projectile(game: Game, position, direction, speed=10):
     # Create a projectile and add it to the projectile group
-    projectile = Projectile(game.spaceship.rect.midtop)
-    game.projectile_group.add(projectile) 
-    
+    projectile = Projectile(position, direction, speed)
+    if direction == 1:
+        game.projectile_group_invaders.add(projectile)
+    elif direction == -1:
+        game.projectile_group_spaceship.add(projectile)
+
 
 def terminate():
     # End pygame and programme
     pygame.quit()
-    sys.exit()    
+    sys.exit()
 
 
 def load_background(DISPLAY_SURFACE):
     # Load the background image and scale it to the size of the window
-    DISPLAY_SURFACE.blit(source=pygame.transform.scale(pygame.image.load(BACKGROUND_IMG).convert_alpha(), size=(WINDOW_HEIGHT, WINDOW_HEIGHT)), dest=(0,0,WINDOW_HEIGHT,WINDOW_WIDTH))
+    DISPLAY_SURFACE.blit(source=pygame.transform.scale(pygame.image.load(BACKGROUND_IMG).convert_alpha(),
+                                                       size=(WINDOW_HEIGHT, WINDOW_HEIGHT)),
+                         dest=(0, 0, WINDOW_HEIGHT, WINDOW_WIDTH))
 
+
+def spaceship_collision(game: Game):
+    value = pygame.sprite.spritecollide(sprite=game.spaceship, group=game.projectile_group_invaders, dokill=True)
+    if value:
+        game.spaceship.health -= 1
+        if game.spaceship.health == 0:
+            print(game.spaceship.health)
+            game.game_active = False
 
 
 def main():
     global FPS_CLOCK, DISPLAY_SURFACE, BASIC_FONT, BUTTONS, BLANK
-    
+
     pygame.init()
     FPS_CLOCK = pygame.time.Clock()
     DISPLAY_SURFACE = pygame.display.set_mode(size=(WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -107,14 +147,14 @@ def main():
     BASIC_FONT = pygame.font.Font('freesansbold.ttf', BASIC_FONT_SIZE)
 
     spaceship = SpaceShip()
-    invaders_list = [Invader(x_pos=x*85, y_pos= y*60) for x in range(1,7) for y in range(1,4)]
+    invaders_list = [Invader(x_pos=x * 85, y_pos=y * 60) for x in range(1, 7) for y in range(1, 4)]
     invader_sprite_group = Group()
     for invader in invaders_list:
         invader_sprite_group.add(invader)
 
-    projectile_sprite_group = Group()  # Create a sprite group for projectiles
-
-    main_game = Game(Invader_group=invader_sprite_group, SpaceShip=spaceship, Projectile_group=projectile_sprite_group)
+    # Create the game object
+    main_game = Game(Invader_group=invader_sprite_group, SpaceShip=spaceship, Projectile_group_inv=Group(),
+                     Projectile_group_spa=Group())
 
     while True:
         for event in pygame.event.get():
@@ -128,8 +168,7 @@ def main():
         invader_sprite_group.draw(DISPLAY_SURFACE)
 
         # Update and draw projectiles
-        main_game.projectile_group.update()  # Update projectile positions
-        main_game.projectile_group.draw(DISPLAY_SURFACE)  # Draw projectiles
+        main_game.update(DISPLAY_SURFACE=DISPLAY_SURFACE)  # Update projectile positions
 
         pygame.display.update()
         FPS_CLOCK.tick(FPS)
